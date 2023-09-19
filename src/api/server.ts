24a23618638543c8ffa32577d1075141babd4f77
Lunixyz <reallytsx@gmail.com>
2023-09-19 13:32:59 -0300

    1.0.1

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,8 +1,8 @@
-import http from "http";
+import https from "http";
 import { app } from "./app";
 import { Base } from "../components/components";
 
-http.createServer(app).listen(3000);
+https.createServer(app).listen(3000);
 console.log("Up & Running.");
 
 const client = new Base();
