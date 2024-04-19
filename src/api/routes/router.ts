1a1e000460fba3abe2383d7ea0c7cc5d6aa88791
Lunixyz <otakinhuhdev@gmail.com>
2024-04-18 21:14:04 -0300

    1.0.13 - rolling back

--- src/api/routes/router.ts
+++ src/api/routes/router.ts
@@ -1,34 +1,16 @@
 import { Router } from "express";
 import root from "app-root-path";
 import { readdirSync } from "fs";
-import path from "path";
 
 export default Router();
 
 class Routes {
   async loadAppRoutes(): Promise<void> {
-    const AppPath = path.resolve(process.cwd(), "src", "api", "routes", "app");
-    const AppRoutes = readdirSync(AppPath);
+    const approutes = readdirSync(`${root}/src/api/routes/app`);
 
-    const AppsPath = path.resolve(
-      process.cwd(),
-      "src",
-      "api",
-      "routes",
-      "apps"
-    );
-    const AppsRoutes = readdirSync(AppsPath);
-
-    for (const file of AppRoutes) {
+    for (const file of approutes) {
       if (file.endsWith("ts")) {
-        const module = await import(`${root}/dist/src/api/routes/app/${file}`);
-        module.default();
-      }
-    }
-
-    for (const file of AppsRoutes) {
-      if (file.endsWith("ts")) {
-        const module = await import(`${root}/dist/src/api/routes/apps/${file}`);
+        const module = await import(`${root}/src/api/routes/app/${file}`);
         module.default();
       }
     }
